527c98a1688498923e0cc567f7abedd06eb416df
Daniel Lee <leedc@me.com>
2019-07-04 15:45:58 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,29 @@
+
+def turn_count(board)
+  turn = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      turn += 1
+    end
+  end
+    return turn
+end
+
+
+
+# def current_player(board)
+#   turn = turn_count(board)
+#     if turn % 2 == 0
+#       return "X"
+#     else
+#       return "O"
+#     end
+# end
+
+# bonus - refactoring #current_player method to use .even? and ternary operator
+
+def current_player(board)
+  turn = turn_count(board)
+    if turn % 2 == 0 ? "X" : "O"
+    end
+end
