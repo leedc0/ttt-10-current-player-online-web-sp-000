58e9e326744538696004651ec07ec4d9a9eb77d1
Daniel Lee <leedc@me.com>
2019-07-04 16:04:57 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,19 +9,17 @@ def turn_count(board)
     return turn
 end
 
-
-
-def current_player(board)
-  turn = turn_count(board)
-    if turn % 2 == 0
-      "X"
-    else
-      "O"
-    end
-end
-
-# bonus - refactoring #current_player method to use .even? and ternary operator
 # def current_player(board)
 #   turn = turn_count(board)
-#     turn = turn.even? ? "X" : "O"
+#     if turn % 2 == 0
+#       "X"
+#     else
+#       "O"
+#     end
 # end
+
+# bonus - refactoring #current_player method to use .even? method & ternary operator
+def current_player(board)
+  turn = turn_count(board)
+    turn = turn.even? ? "X" : "O"
+end
